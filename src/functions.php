604cade550474de1de48906b5399cbb70412b8c7
GuryanovaIrene <44869308+GuryanovaIrene@users.noreply.github.com>
2018-11-26 03:53:46 +0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,130 +1,114 @@
 <?php
 function task1($arr, $flag = false) {
+    foreach ($arr as $val) {
+        echo '<p>' . $val . '</p>';
+    }
     if ($flag) {
         return implode(',', $arr);
-    } else {
-        foreach ($arr as $val) {
-            echo '<p>' . $val . '</p>';
-        }
     }
 }
+
 function task2($oper) {
-    $i = 0;
-    foreach (func_get_args() as $value) {
-        if ($i > 0) {
-            if ($i == 1) {
-                $res = $value;
-            } else {
-                switch ($oper) {
-                    case '+':
-                        $res = $res + $value;
-                        break;
-                    case '-':
-                        $res = $res - $value;
-                        break;
-                    case '*':
-                        $res = $res * $value;
-                        break;
-                    case '/':
-                        if ($value == 0) {
-                            return 'Деление на 0!!!';
-                        }
-                        $res = $res / $value;
-                        break;
-                    default:
-                        return 'Неизвестная операция!!!';
+    if (!($oper == '+' or $oper == '-' or $oper == '*' or $oper == '/')) {
+        return 'Неизвестная операция!!!';
+    }
+    $args = func_get_args();
+    print_r($args);
+    array_splice($args, 0, 1);
+    print_r($args);
+    $res = $args[0];
+    array_splice($args, 0, 1);
+    print_r($args);
+    foreach ($args as $value) {
+        switch ($oper) {
+            case '+':
+                $res = $res + $value;
+                break;
+            case '-':
+                $res = $res - $value;
+                break;
+            case '*':
+                $res = $res * $value;
+                break;
+            case '/':
+                if ($value == 0) {
+                    return 'Деление на 0!!!';
                 }
-            }
+                $res = $res / $value;
+                break;
         }
-        $i++;
     }
     return $res;
 }
+
 function task3($n, $m) {
     //  Таблица умножения $n строк $m столбцов
-    $nType = gettype($n) == 'integer';
-    $nNatural = $n >= 1;
-    $mType = gettype($m) == 'integer';
-    $mNatural = $m >= 1;
-    if ($nType and $nNatural and $mType and $mNatural) {
-        // Если ввод данных корректен, формируем таблицу
-        echo '
+    if (!($n > 0)) {
+        return 'Первый параметр не является положительным числом';
+    }
+    if (!($m > 0)) {
+        return 'Второй параметр не является положительным числом';
+    }
+    // Если ввод данных корректен, формируем таблицу
+    echo '
 <table>
     <thead>
         <tr>    
             <th>&nbsp;</th>';
-        for ($j = 1; $j <= $m; $j++) {
-            echo '<th>' . $j . '</th>';
-        }
-        echo '
+    for ($j = 1; $j <= $m; $j++) {
+        echo '<th>' . $j . '</th>';
+    }
+    echo '
         </tr>
     </thead>
     <tbody>';
-        for ($i = 1; $i <= $n; $i++) {
-            echo '<tr><td>' . $i . '</td>';
-            for ($j = 1; $j <= $m; $j++) {
-                echo '<td>' . $i * $j . '</td>';
-            }
-            echo '</tr>';
+    for ($i = 1; $i <= $n; $i++) {
+        echo '<tr><td>' . $i . '</td>';
+        for ($j = 1; $j <= $m; $j++) {
+            echo '<td>' . $i * $j . '</td>';
         }
-        echo '
+        echo '</tr>';
+    }
+    echo '
     </tbody>
 </table>';
-        return 'Параметры введены корректно';
-    } else {
-        // Если данные некорректны, сформулируем ошибки и вернем результат
-        $res = '';
-        if (!$nType) {
-            $res = $res . 'Первый параметр не является целым числом' . PHP_EOL;
-        }
-        if (!$nNatural) {
-            $res = $res . 'Первый параметр не является положительным числом' . PHP_EOL;
-        }
-        if (!mType) {
-            $res = $res . 'Второй параметр не является целым числом' . PHP_EOL;
-        }
-        if (!$mNatural) {
-            $res = $res . 'Второй параметр не является положительным числом' . PHP_EOL;
-        }
-        return $res;
-    }
+    return 'Параметры введены корректно';
 }
+
 function task4() {
     return date('d.m.Y H:i');
 }
+
 function task5() {
     return mktime(0, 0, 0, 2, 24, 2016);
 }
+
 function task6($str) {
     return str_replace('К', '', $str);
 }
+
 function task7($str) {
     return str_replace('Две', 'Три', $str);
 }
+
 function task8($fileName, $str) {
     //  Создание файла с именем $fileName и запись в файл строки $str
-    if ($f = fopen($fileName,'w')) {
+    $f = fopen($fileName,'w');
+    if ($f) {
         fwrite($f, $str);
     } else {
-        task10('W');
+        echo 'Не удалось открыть файл на запись';
     }
+    fclose($f);
 }
+
 function task9($fileName) {
-    if ($f = fopen($fileName,'rb')) {
-        $res = fread($f);
+    $f = fopen($fileName,'r');
+    if ($f) {
+        $res = fread($f, filesize($fileName));
         echo $res;
     } else {
-        task10('R');
+        echo 'Не удалось открыть файл на чтение';
     }
-}
-function task10($exceptionType) {
-    // Обработка исключений
-    switch ($exceptionType) {
-        case 'W':
-            echo 'Не удалось произвести запись в файл';
-            break;
-        case 'R':
-            echo 'Не удалось прочитать файл';
-            break;
-    }
+    fclose($f);
 }
